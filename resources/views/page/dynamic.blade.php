15e862c530a7164231d83f9cdaf9b7986471a1c9
xiaoheixi <justinzhao893@gmail.com>
2020-10-01 17:59:17 +1000

    3rd commit! Fixed Admin Login Page!

--- resources/views/page/dynamic.blade.php
+++ resources/views/page/dynamic.blade.php
@@ -3,19 +3,21 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('css/main.css') }}" /> 
+        <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
     </head>
+    @if ($pageContent->URI != 'adminLogin')
     @component('layouts.components.navbar')
     @endcomponent
-    <body> 
+    @endif
+    <body>
         @if ($pageContent->URI == 'home')
             <style>
                 .page-title-header{
                     background: transparent;
-                    padding: 20px; 
+                    padding: 20px;
                     font-size: 15px;
                 }
-                .page-title-header p{ 
+                .page-title-header p{
                     font-size: 15px;
                     text-transform: capitalize;
                     letter-spacing: 1px;
@@ -33,11 +35,11 @@
         @if ($pageContent->URI == 'home')
             @component('layouts.components.radioShow')
             @endcomponent
-        @endif 
+        @endif
 
         <div class="page-title-header">
             {!!$pageContent->pageContent!!}
-        </div> 
+        </div>
         @if ($pageContent->URI == 'shop')
             @component('layouts.components.sidenavbar')
             @endcomponent
@@ -97,9 +99,13 @@
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        @if ($pageContent->URI != 'adminLogin')
         @component('layouts.components.contact')
-        @endcomponent 
+        @endcomponent
+        @endif
     </body>
+    @if ($pageContent->URI != 'adminLogin')
     @component('layouts.components.footer')
     @endcomponent
+    @endif
 </html>
