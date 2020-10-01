9d368ed42b8b95a6f19547b30c2cc22bbc8d0c4d
xiaoheixi <justinzhao893@gmail.com>
2020-10-01 19:56:27 +1000

    10th commit! Fixed ContactDetailsController.php!

--- app/Http/Controllers/ContactDetailsController.php
+++ app/Http/Controllers/ContactDetailsController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\contactDetails;
-use DB;
-use Illuminate\Database\MySqlConnection;
+use Illuminate\Support\Facades\DB;
 
 class ContactDetailsController extends Controller
 {
