71db24e26c50f50c262d04a85f47781831414f82
xiaoheixi <justinzhao893@gmail.com>
2020-10-01 23:41:57 +1000

    34th commit!

--- resources/views/dynamic_email.blade.php
+++ resources/views/dynamic_email.blade.php
@@ -1,3 +1,4 @@
 <p>Hi, This is {{ $data['name'] }}</p>
-<p>I have some query like {{ $data['message'] }}</p>
+<p>{{ $data['message'] }}</p>
 <p> It would be appreciative, if you have gone through this feedback.</p>
+<p>Sent from: {{ $data['email']}}</p>
