60426e42e9acca1099699280d0f6945861e3ad21
xiaoheixi <justinzhao893@gmail.com>
2020-10-01 20:14:10 +1000

    20th commit! SideNavController.php fixed!

--- app/Http/Controllers/SideNavController.php
+++ app/Http/Controllers/SideNavController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\sideNav;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\MySqlConnection;
 
 class SideNavController extends Controller
 {
