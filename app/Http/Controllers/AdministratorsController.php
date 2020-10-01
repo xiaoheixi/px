18bfa6886639471385c7bc64f22b843a69c8580b
xiaoheixi <justinzhao893@gmail.com>
2020-10-01 19:52:36 +1000

    7th Commit! Fixed AdministratorsController.

--- app/Http/Controllers/AdministratorsController.php
+++ app/Http/Controllers/AdministratorsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\administrators;
-use DB;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\MySqlConnection;
 
 class AdministratorsController extends Controller
