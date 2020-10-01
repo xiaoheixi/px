b5c9dccd2514409531866618e877a3a266b5985c
xiaoheixi <justinzhao893@gmail.com>
2020-10-01 19:57:21 +1000

    11th commit! Fixed FooterController.php

--- app/Http/Controllers/FooterController.php
+++ app/Http/Controllers/FooterController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\footer;
-use DB;
-use Illuminate\Database\MySqlConnection;
+use Illuminate\Support\Facades\DB;
 class FooterController extends Controller
 {
     public function index()
