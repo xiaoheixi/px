adf0360f84126078224de474d81057a5402edca1
xiaoheixi <justinzhao893@gmail.com>
2020-10-03 15:05:39 +1000

    39th commit!

--- resources/views/layouts/components/send_email.blade.php
+++ resources/views/layouts/components/send_email.blade.php
@@ -1,33 +1,33 @@
 @if(count($errors) > 0)
-    <div class="alert alert-danger">
-        <button type="button" class="close" data-dismiss="alert">x</button>
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+<div class="alert alert-danger">
+    <button type="button" class="close" data-dismiss="alert">x</button>
+    <ul>
+        @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
 @endif
 @if($message = Session::get('success'))
-    <div class="alert alert-success alert-block">
-        <button type="button" class="close" data-dismiss="alert">x</button>
-        <strong>{{ $message }}</strong>
-    </div>
+<div class="alert alert-success alert-block">
+    <button type="button" class="close" data-dismiss="alert">x</button>
+    <strong>{{ $message }}</strong>
+</div>
 @endif
-<form method="post" action="{{ url('sendemail/send') }}">
+<form class="formThankYou" method="post" action="{{ url('sendemail/send') }}">
     {{ csrf_field() }}
     <div class="form-group">
         <label>Enter Your Name</label>
-        <input type="text" name="name" class=" form-control" />
+        <input type="text" name="name" class=" form-controlThankYou" />
     </div>
     <div class="form-group">
         <label>Enter Your Email</label>
-        <input type="text" name="email" class="form-control" />
+        <input type="text" name="email" class="form-controlThankYou" />
     </div>
     <div class="form-group">
         <label>Enter Your Message</label>
-        <textarea name="message" class="form-control"></textarea>
+        <textarea name="message" class="form-controlThankYouMessage"></textarea>
     </div>
     <div class="form-groupFooter">
-        <input type="submit" name="send" value="Send" class="btn btn-info"/>
+        <input type="submit" name="send" value="Send" class="btn btn-info" />
 </form>
--- resources/views/page/dynamic.blade.php
+++ resources/views/page/dynamic.blade.php
@@ -1,119 +1,133 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-    </head>
-    @if ($pageContent->URI != 'adminLogin')
-    @component('layouts.components.navbar')
-    @endcomponent
-    @endif
-    <body>
-        @if ($pageContent->URI == 'home')
-            <style>
-                .page-title-header{
-                    background: transparent;
-                    padding: 20px;
-                    font-size: 15px;
-                }
-                .page-title-header p{
-                    font-size: 15px;
-                    text-transform: capitalize;
-                    letter-spacing: 1px;
-                    font-family: sans-serif;
-                    text-align: justify !important;
-                    /* padding: 10px 80px 10px 20px; */
-                }
-                body{
-                    overflow-x: hidden;
-                }
-            </style>
-            @component('layouts.components.carousel')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'home')
-            @component('layouts.components.radioShow')
-            @endcomponent
-        @endif
 
-        <div class="page-title-header">
-            {!!$pageContent->pageContent!!}
-        </div>
-        @if ($pageContent->URI == 'shop')
-            @component('layouts.components.sidenavbar')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'videos')
-            @component('layouts.components.sidenavbar')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'audio')
-            @component('layouts.components.sidenavbar')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'books')
-            @component('layouts.components.sidenavbar')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'cart')
-            @component('layouts.components.cart')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'checkOut')
-            @component('layouts.components.checkout')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'sendemail')
-            @component('layouts.components.send_email')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'adminLogin')
-            @component('layouts.components.adminLogin')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'service')
-            @component('layouts.components.service')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'news')
-            @component('layouts.components.news')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'shop')
-            @component('layouts.components.shop')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'audio')
-            @component('layouts.components.audioShop')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'books')
-            @component('layouts.components.bookShop')
-            @endcomponent
-        @endif
-        @if ($pageContent->URI == 'videos')
-            @component('layouts.components.videoShop')
-            @endcomponent
-        @endif
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-        @if ($pageContent->URI != 'adminLogin')
-        @component('layouts.components.contact')
-        @endcomponent
-        @endif
-    </body>
-    @if ($pageContent->URI == 'thankYou')
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+</head>
+@if ($pageContent->URI != 'adminLogin')
+@component('layouts.components.navbar')
+@endcomponent
+@endif
+
+<body>
+    @if ($pageContent->URI == 'home')
     <style>
-        .footer-bottom{
-            margin-top: 411px;
+        .page-title-header {
+            background: transparent;
+            padding: 20px;
+            font-size: 15px;
         }
+
+        .page-title-header p {
+            font-size: 15px;
+            text-transform: capitalize;
+            letter-spacing: 1px;
+            font-family: sans-serif;
+            text-align: justify !important;
+            /* padding: 10px 80px 10px 20px; */
+        }
+
+        body {
+            overflow-x: hidden;
+        }
+
     </style>
-    @endif
-    @if ($pageContent->URI != 'adminLogin')
-    @component('layouts.components.footer')
+    @component('layouts.components.carousel')
     @endcomponent
     @endif
+    @if ($pageContent->URI == 'home')
+    @component('layouts.components.radioShow')
+    @endcomponent
+    @endif
+
+    <div class="page-title-header">
+        {!!$pageContent->pageContent!!}
+    </div>
+    @if ($pageContent->URI == 'shop')
+    @component('layouts.components.sidenavbar')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'videos')
+    @component('layouts.components.sidenavbar')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'audio')
+    @component('layouts.components.sidenavbar')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'books')
+    @component('layouts.components.sidenavbar')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'cart')
+    @component('layouts.components.cart')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'checkOut')
+    @component('layouts.components.checkout')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'sendemail')
+    @component('layouts.components.send_email')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'adminLogin')
+    @component('layouts.components.adminLogin')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'service')
+    @component('layouts.components.service')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'news')
+    @component('layouts.components.news')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'shop')
+    @component('layouts.components.shop')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'audio')
+    @component('layouts.components.audioShop')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'books')
+    @component('layouts.components.bookShop')
+    @endcomponent
+    @endif
+    @if ($pageContent->URI == 'videos')
+    @component('layouts.components.videoShop')
+    @endcomponent
+    @endif
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    </script>
+    @if ($pageContent->URI != 'adminLogin')
+    @component('layouts.components.contact')
+    @endcomponent
+    @endif
+</body>
+@if ($pageContent->URI == 'thankYou')
+<style>
+    .footer-bottom {
+        margin-top: 411px;
+    }
+
+</style>
+@endif
+@if ($pageContent->URI != 'adminLogin')
+@component('layouts.components.footer')
+@endcomponent
+@endif
+
 </html>
