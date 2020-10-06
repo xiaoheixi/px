3f3e593e8cd987c85678e646aa926e9e481e9513
xiaoheixi <justinzhao893@gmail.com>
2020-10-06 16:41:27 +1100

    50th commit!

--- resources/views/layouts/components/adminSideNavBar.blade.php
+++ resources/views/layouts/components/adminSideNavBar.blade.php
@@ -1,7 +1,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="{{ asset('css/main.css') }}" />  
+<link rel="stylesheet" href="{{ asset('css/main.css') }}" />
 <div class="admin_sidebar">
-<h1><a href="/">Marketing 4 growth</a></h1>
+    <h1><a href="/">Marketing4growth</a></h1>
     <ul>
         @foreach($adminSideNavContent as $adminSideNav)
         <li>
@@ -10,4 +10,3 @@
         @endforeach
     </ul>
 </div>
-
