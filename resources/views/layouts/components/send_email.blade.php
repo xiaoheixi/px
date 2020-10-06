a4c74d4da811fca7d7e134e5eff497d3b8bd8ca9
xiaoheixi <justinzhao893@gmail.com>
2020-10-06 20:44:45 +1100

    52nd commit!

--- resources/views/layouts/components/send_email.blade.php
+++ resources/views/layouts/components/send_email.blade.php
@@ -1,37 +1,41 @@
-@if(count($errors) > 0)
-<div class="alert alert-danger">
-    <button type="button" class="close" data-dismiss="alert">x</button>
-    <ul>
-        @foreach($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
-@if($message = Session::get('success'))
-<div class="alert alert-success alert-block">
-    <button type="button" class="close" data-dismiss="alert">x</button>
-    <strong>{{ $message }}</strong>
-</div>
-@endif
-<div class="container">
-    <div class="row justify-content-center">
-        <form class="form-container" method="post" action="{{ url('sendemail/send') }}">
-            {{ csrf_field() }}
-            <div class="form-group">
-                <label for="name">Enter Your Name</label><br>
-                <input type="text" name="name" class=" form-controlThankYou" />
+<div class="service-page page">
+    <div class="container">
+        <div class="row align-items-center">
+            <Div class="col-md-6 " style="margin-top: 50px;margin-bottom: 40px;">
+                @if(count($errors) > 0)
+                <div class="alert alert-danger">
+                    <button type="button" class="close" data-dismiss="alert">x</button>
+                    <ul>
+                        @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+                @if($message = Session::get('success'))
+                <div class="alert alert-success alert-block">
+                    <button type="button" class="close" data-dismiss="alert">x</button>
+                    <strong>{{ $message }}</strong>
+                </div>
+                @endif
+                <form method="post" action="{{ url('sendemail/send') }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label>Enter Your Name</label>
+                        <input type="text" name="name" class=" form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Enter Your Email</label>
+                        <input type="text" name="email" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Enter Your Message</label>
+                        <textarea name="message" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="send" value="Send" class="btn btn-info" />
+                </form>
             </div>
-            <div class="form-group">
-                <label for="email">Enter Your Email</label><br>
-                <input type="text" name="email" class="form-controlThankYou" />
-            </div>
-            <div class="form-group">
-                <label for="message">Enter Your Message</label><br>
-                <textarea name="message" class="form-controlThankYouMessage" placeholder="Write something.."></textarea>
-            </div>
-            <div class="form-group">
-                <input type="submit" name="send" value="Send" class="btn btn-info btn-block" />
-        </form>
+        </Div>
     </div>
 </div>
